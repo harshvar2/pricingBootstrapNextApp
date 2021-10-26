069dbd311545ecfdada7b73acc4c2ea25a922d9e
Harshvardhan Rajdan <harj220@gmail.com>
2021-10-26 19:51:38 +0530

    fixed changes

--- components/HomePage.js
+++ components/HomePage.js
@@ -2,72 +2,22 @@
 import { BsCheckLg } from "react-icons/bs";
 //Importing bootstrap and other modules
 import 'bootstrap/dist/css/bootstrap.min.css';
+import PricingTable from './PricingTable'
+import { Container, Col, Row } from "react-bootstrap";
 function HomePage() {
   return (
     <>
 
-      <div className="pricing-header  pb-md-3  text-center col-xs-12 col-lg-8 m-auto">
-        <h1 className="display-4 fw-normal">Pricing</h1>
-        <p className="fs-5 text-muted ">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It's built with default Bootstrap components and utilities with little customization.</p>
-
-      </div>
-      <div className="container">
-        <div class="row   text-center">
-          <div className="col-xs-12 col-md-4 box" >
-            <div className="card mb-4 box-shadow">
-              <div className="card-header">
-                <h4 className="my-0 font-weight-normal">Free</h4>
-              </div>
-              <div className="card-body">
-                <h1 className="card-title pricing-card-title">$0 <small className="text-muted">/ mo</small></h1>
-                <ul className="list-unstyled mt-3 mb-4">
-                  <li>10 users included</li>
-                  <li>2 GB of storage</li>
-                  <li>Email support</li>
-                  <li>Help center access</li>
-                </ul>
-                <button type="button" className="btn w-100 btn-lg btn-block btn-outline-primary">Sign up for free</button>
-              </div>
-            </div>
-          </div>
-          <div className="col-xs-12 col-md-4 box ">
-            <div className="card mb-4 box-shadow">
-              <div className="card-header">
-                <h4 className="my-0 font-weight-normal">Pro</h4>
-              </div>
-              <div className="card-body">
-                <h1 className="card-title pricing-card-title">$15 <small className="text-muted">/ mo</small></h1>
-                <ul className="list-unstyled mt-3 mb-4">
-                  <li>20 users included</li>
-                  <li>10 GB of storage</li>
-                  <li>Priority email support</li>
-                  <li>Help center access</li>
-                </ul>
-                <button type="button" className="w-100 btn btn-lg btn-block btn-primary">Get started</button>
-              </div>
-            </div>
-          </div>
-          <div className="col-xs-12 col-md-4 box">
-            <div class="card mb-4 box-shadow border-primary">
-              <div class="card-header text-white bg-primary border-primary">
-                <h4 class="my-0 font-weight-normal">Enterprise</h4>
-              </div>
-              <div class="card-body">
-                <h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ mo</small></h1>
-                <ul class="list-unstyled mt-3 mb-4">
-                  <li>30 users included</li>
-                  <li>15 GB of storage</li>
-                  <li>Phone and email support</li>
-                  <li>Help center access</li>
-                </ul>
-                <button type="button" class=" w-100 btn btn-lg btn-block btn-primary">Contact us</button>
-              </div>
-            </div>
-          </div>
-        </div>
-
-
-
+      <Container  >
+        <Row className="justify-content-md-center">
+          <Col xs="12" lg="8" className="text-center">
+            <h1 className="display-4 fw-normal">Pricing</h1>
+            <p className="fs-5 text-muted ">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It's built with default Bootstrap components and utilities with little customization.</p>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <PricingTable />
         <h2 className="display-6 text-center mt-3 mb-3">Compare plans</h2>
         <div className="table-responsive">
           <table className="table text-center">
@@ -120,7 +70,7 @@ function HomePage() {
                 <td className="px-3"><BsCheckLg /></td>
               </tr>
             </tbody>
-          </table></div></div>
+          </table></div></Container>
     </>
   )
 }
--- components/Navigation.js
+++ components/Navigation.js
@@ -1,17 +1,19 @@
 import { Navbar, Container } from 'react-bootstrap'
+import Image from 'next/image'
 function Navigation() {
     return (
-       
-            <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-4 bg-white border-bottom ">
-            <Navbar.Brand href="#">
 
-                <img
-                    alt=""
-                    src="/bootstrap-logo.svg"
-                    width="40"
-                    height="45"
-                    className="me-3 py-2"
-                />
+        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-4 bg-white border-bottom ">
+            <Navbar.Brand href="#" className="d-flex md-auto justify-content-end">
+                <div style={{ width: "3rem", height: "3rem",paddingTop: "0.3rem",marginRight: "0.3rem"}}>
+                    <Image
+                        alt=""
+                        src="/bootstrap-logo.svg"
+                        width={2400}
+                        height={1598}
+                        layout="responsive"
+                    />
+                </div>
                 <a href="#" className="me-3 py-2 text-dark text-decoration-none">Pricing Example</a>
             </Navbar.Brand>
             <nav className=" d-flex ms-md-auto justify-content-end">
